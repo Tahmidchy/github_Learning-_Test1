5505a302916384e17b7c74e4b9aaf4377670d724
A.T.M.MUSTAFA MASUD CHOWDHURY <Tahmidchy@users.noreply.github.com>
2026-01-27 20:26:05 +0600

    Tahmid Chowdhury With String Method 1
    Here we are solve String Method of JavaScript

---
+++ Problem4.js
@@ -0,0 +1,9 @@
+/*
+TODO: Program-4: Create a string in the language whose value will be JavaScript. This time, check with indexOf, if any index has been started.
+*/
+
+//Solution: 
+
+const language = 'JavaScript';
+const index = language.indexOf('rip'); // Check for the substring 'rip'
+console.log(index); // Output: 4 (since 'rip' starts at index 4)
---
+++ Problem6.js
@@ -0,0 +1,9 @@
+/*
+TODO: Problem-6: There is an email const email = 'User@Example.Com'; Convert this email to lowercase.
+*/
+
+// Solution: 
+
+const email = 'User@Example.Com';
+const lowerCaseEmail = email.toLowerCase(); // Convert the email to lowercase
+console.log(lowerCaseEmail); // Output: ' 
