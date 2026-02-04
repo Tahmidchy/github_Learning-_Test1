d2ea209952bec2096357ae530edbd338b69f970c
A.T.M.MUSTAFA MASUD CHOWDHURY <Tahmidchy@users.noreply.github.com>
2026-02-04 21:39:28 +0600

    Tahmid Chowdhury JavaScript Error Handling
    In here, JavaScript Error Handling

--- Problem2.js
+++ Problem2.js
@@ -1,7 +1,19 @@
 /*
-TODO: Problem-2: Declare a variable named "learning" and set its value to true. Use the typeof operator to determine its output.
- */
-//Solution: 
+TODO: Problem-2: Write a function that will return the last element of an array, or give a warning message if the input is not an array.
+*/
 
-let learning = true;
-console.log(typeof learning); // Output: boolean
+// valid Type Validation
+
+function getLastElement(arr){
+    if(!Array.isArray(arr)){
+        return "Please provide a valid array";
+    }
+    return arr[arr.length - 1];
+}
+const array1 = [1, 2, 3, 4, 5];
+const output1 = getLastElement(array1);
+console.log(output1); // 5
+// invalid Type Validation
+const array2 = "Not an array";
+const output2 = getLastElement(array2);
+console.log(output2); // Please provide a valid array
